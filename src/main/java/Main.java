e4cf454da0241fc3bc6b93aba56be02ac6f5d69a
ericliu0000 <55063842+ericliu0000@users.noreply.github.com>
2020-09-16 15:08:13 -0400

    Make graph in main
    Moved graph stuff from fxml to the main class, seems to be more difficult to edit graph parameters in fxml.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,79 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.beans.binding.ObjectExpression;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class Main extends Application {
-    public static Parent mainRoot;
-
-    static {
-        try {
-            mainRoot = FXMLLoader.load(Main.class.getResource("main.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public static Scene mainScene = new Scene(mainRoot);
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Main");
-        stage.setScene(mainScene);
+        stage.setTitle("Collatz Conjecture Visualization");
+
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+
+        xAxis.setLabel("Iteration");
+        yAxis.setLabel("Value of number in cycle");
+
+        final LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+
+        lineChart.setTitle("Collatz Conjecture Visualization");
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Value");
+
+        ArrayList<Double> collatzData = collatz(50);
+
+        for (int i = 1; i < collatzData.size(); i++) {
+            series.getData().add(new XYChart.Data(i, collatzData.get(i)));
+        }
+
+        Scene scene = new Scene(lineChart, 800, 600);
+        lineChart.getData().add(series);
+
+        stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
+    private static ArrayList<Double> collatz(double num) {
+        ArrayList<Double> proc = new ArrayList<>();
+        proc.add(num);
+
+        while (true) {
+            if (num == 1) {
+                break;
+            }
+            if (num % 2 == 1) {
+                num = (num * 3) + 1;
+            } else {
+                num /= 2;
+            }
+            proc.add(num);
+//            System.out.println(num);
+        }
+        return proc;
+    }
+
+    /* wip label atop each point
+    private static Node dataLabel(ObjectExpression<Number> val) {
+        Label label = new Label();
+        label.textProperty().bind(val.asString("%d"));
+
+        Pane pane = new Pane(label);
+        label.textProperty()
+    }
+     */
 }
--- src/main/java/MainController.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.chart.Chart;
-
-public class MainController {
-    public Chart mainChart;
-
-    public static void main(String[] args) {
-
-    }
-}
